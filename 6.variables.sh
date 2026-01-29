8ec3239206160d14461ef5cd150a5de3abd594c6
sai <varunsai6303@gmail.com>
2026-01-29 22:16:29 +0530

    actions

--- 6.variables.sh
+++ 6.variables.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-START_TIME=$(date+%s)
+START_TIME=$(date +%s)
 
 echo "script executed at: $START_TIME"
 
 SLEEP 10
 
-END_TIME=$(date+%s)
+END_TIME=$(date +%s)
 
-TOTAL_TIME=$(START_TIME-END_TIME)
+TOTAL_TIME=$((END_TIME-START_TIME))
 
 echo "script executed in :$TOTAL_TIME SECONDS"
+
+
